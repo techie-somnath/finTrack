e3439b03f68eb6b3b80f57a6580529825046f897
Somnath Goswami <somnath.goswami@cginfinity.com>
2025-01-08 00:41:50 +0530

    implement file and folder structure

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,195 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+/* eslint-disable no-prototype-builtins */
+import { type ClassValue, clsx } from "clsx";
+import qs from "query-string";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// FORMAT DATE TIME
+export const formatDateTime = (dateString: Date) => {
+  const dateTimeOptions: Intl.DateTimeFormatOptions = {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  };
+
+  const dateDayOptions: Intl.DateTimeFormatOptions = {
+    weekday: "short",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  };
+
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    month: "short",
+    year: "numeric",
+    day: "numeric",
+  };
+
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  };
+
+  const formattedDateTime: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateTimeOptions
+  );
+
+  const formattedDateDay: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateDayOptions
+  );
+
+  const formattedDate: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateOptions
+  );
+
+  const formattedTime: string = new Date(dateString).toLocaleString(
+    "en-US",
+    timeOptions
+  );
+
+  return {
+    dateTime: formattedDateTime,
+    dateDay: formattedDateDay,
+    dateOnly: formattedDate,
+    timeOnly: formattedTime,
+  };
+};
+
+export function formatAmount(amount: number): string {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  });
+
+  return formatter.format(amount);
+}
+
+export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
+
+export const removeSpecialCharacters = (value: string) => {
+  return value.replace(/[^\w\s]/gi, "");
+};
+
+interface UrlQueryParams {
+  params: string;
+  key: string;
+  value: string;
+}
+
+export function formUrlQuery({ params, key, value }: UrlQueryParams) {
+  const currentUrl = qs.parse(params);
+
+  currentUrl[key] = value;
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  );
+}
+
+export function getAccountTypeColors(type: AccountTypes) {
+  switch (type) {
+    case "depository":
+      return {
+        bg: "bg-blue-25",
+        lightBg: "bg-blue-100",
+        title: "text-blue-900",
+        subText: "text-blue-700",
+      };
+
+    case "credit":
+      return {
+        bg: "bg-success-25",
+        lightBg: "bg-success-100",
+        title: "text-success-900",
+        subText: "text-success-700",
+      };
+
+    default:
+      return {
+        bg: "bg-green-25",
+        lightBg: "bg-green-100",
+        title: "text-green-900",
+        subText: "text-green-700",
+      };
+  }
+}
+
+export function countTransactionCategories(
+  transactions: Transaction[]
+): CategoryCount[] {
+  const categoryCounts: { [category: string]: number } = {};
+  let totalCount = 0;
+
+  // Iterate over each transaction
+  transactions &&
+    transactions.forEach((transaction) => {
+      // Extract the category from the transaction
+      const category = transaction.category;
+
+      // If the category exists in the categoryCounts object, increment its count
+      if (categoryCounts.hasOwnProperty(category)) {
+        categoryCounts[category]++;
+      } else {
+        // Otherwise, initialize the count to 1
+        categoryCounts[category] = 1;
+      }
+
+      // Increment total count
+      totalCount++;
+    });
+
+  // Convert the categoryCounts object to an array of objects
+  const aggregatedCategories: CategoryCount[] = Object.keys(categoryCounts).map(
+    (category) => ({
+      name: category,
+      count: categoryCounts[category],
+      totalCount,
+    })
+  );
+
+  // Sort the aggregatedCategories array by count in descending order
+  aggregatedCategories.sort((a, b) => b.count - a.count);
+
+  return aggregatedCategories;
+}
+
+export function extractCustomerIdFromUrl(url: string) {
+  // Split the URL string by '/'
+  const parts = url.split("/");
+
+  // Extract the last part, which represents the customer ID
+  const customerId = parts[parts.length - 1];
+
+  return customerId;
+}
+
+export function encryptId(id: string) {
+  return btoa(id);
+}
+
+export function decryptId(id: string) {
+  return atob(id);
+}
+
+export const getTransactionStatus = (date: Date) => {
+  const today = new Date();
+  const twoDaysAgo = new Date(today);
+  twoDaysAgo.setDate(today.getDate() - 2);
+
+  return date > twoDaysAgo ? "Processing" : "Success";
+};
